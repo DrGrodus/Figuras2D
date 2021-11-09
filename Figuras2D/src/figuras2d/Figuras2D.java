9ec1b35fdc23a1d054be51a63d228d747f1cbd67
DrGrodus <ebermd00@gmail.com>
2021-11-08 21:58:05 -0500

    Se agregó el proyecto

---
+++ Figuras2D/src/figuras2d/Figuras2D.java
@@ -0,0 +1,12 @@
+package figuras2d;
+
+/**
+ *
+ * @author Eber MD
+ */
+public class Figuras2D {
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ Figuras2D/src/figuras2d/Shape.java
@@ -0,0 +1,11 @@
+package figuras2d;
+
+/**
+ *
+ * @author Eber MD
+ */
+public interface Shape {
+    public double getArea();
+    
+    public float getPerimeter();
+}
